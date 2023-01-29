6027cbab666bd6ebb846ba61324c21b4c5d2a4f7
Alevtyna777 <119439779+Alevtyna777@users.noreply.github.com>
2023-01-29 23:08:48 +0200

    Homework # 15.

---
+++ src/ClassicMusic.java
@@ -0,0 +1,9 @@
+public class ClassicMusic extends MusicStyles{
+
+ @Override
+ public void playMusic() {
+
+
+        System.out.println("Playing Classic music");
+        }
+        }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
+        MusicStyles[] groups = {new RockMusic(), new ClassicMusic(), new PopMusic()};
+        for (MusicStyles musicStyles : groups) {
+            musicStyles.playMusic();
+        }
     }
 }
